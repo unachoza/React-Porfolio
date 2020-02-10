ada82db45f67dd3b869d47e5410b41096770de90
arianna choza <unachoza@gmail.com>
2020-02-10 01:56:14 -0500

    data file, keeping away from components to ensure they are reusable

---
+++ src/Components/ProjectCard/project-data.js
@@ -0,0 +1,84 @@
+// Project Image
+// Project Title
+// Project Description
+// Project Code link
+
+const projectData = [
+  {
+    id: 1,
+  name: 'Susan Says',
+    
+projectLink: '',    
+imageLinks: [
+      'https://res.cloudinary.com/dh41vh9dx/image/upload/v1553110722/Screen_Shot_2019-03-20_at_3.38.20_PM.png',
+      'https://res.cloudinary.com/dh41vh9dx/image/upload/c_scale,h_140,w_230/v1546220914/susan-sarandon.jpg',
+    ],
+    description:
+      'Interactive arcade style memory game using vanilla javascript, CSS and Responsive Web Design. Users repeat a color pattern delivered by "Susan" that gets infinitely longer as they continue to repeat correctly. Upon arriving to each new level, the background changes to a differnt feminist. Users recieve a score at the end. Be ware, it\'s addictive!',
+    githubLink: 'https://github.com/unachoza/Susan-Says',
+  },
+  {
+    id: 2,
+    name: 'Cancel R Kelly',
+projectLink: '',    
+imageLinks: [
+      'https://res.cloudinary.com/dh41vh9dx/image/upload/v1574386294/Screen_Shot_2019-11-21_at_8.31.22_PM.png',
+    ],
+    description:
+      'Cancel RKelly is an app that effortlessly allows users remove Rkelly songs from their Spotify Playists and Library. This app populates users playlists, then filters through to find playlists containing R Kelly songs and promps the user to delete them. Using the Spotify API, React, Nodejs, Express, and PostgreSQL.',
+
+    githubLink: 'https://github.com/unachoza/Cancel-RKelly-with-Spotify-Aut',
+  },
+  {
+    id: 3,
+    name: 'Pinnacle Clothing',
+projectLink: '',    
+imageLinks: [
+      'https://res.cloudinary.com/dh41vh9dx/image/upload/v1580794434/Screen_Shot_2020-02-04_at_12.33.27_AM.png',
+      'https://res.cloudinary.com/dh41vh9dx/image/upload/v1580794434/Screen_Shot_2020-02-04_at_12.32.36_AM.png',
+    ],
+    description:
+      'E-commerce site using Firebase Authentication. Built using React and Redux, integrated with Stripe API to accept credit or debt payments. This is a fictional online clothing store, but gosh does it look great!',
+
+    githubLink: 'https://github.com/unachoza/e-commerce',
+  },
+  {
+    id: 4,
+    name: 'Face Recognition AI<',
+projectLink: '',    
+imageLinks: [
+      'https://res.cloudinary.com/dh41vh9dx/image/upload/v1580792102/Screen_Shot_2020-02-03_at_11.49.54_PM.png',
+      'https://res.cloudinary.com/dh41vh9dx/image/upload/v1580792102/Screen_Shot_2020-02-03_at_11.53.31_PM.png',
+    ],
+    description:
+      'Artificial Intelligence is used to detect faces in images. Users enter image urls and AI does the rest!',
+    githubLink: 'https://github.com/unachoza/Recognition-Fullstack-',
+  },
+  {
+    id: 5,
+    name: 'Github Respo Search',
+projectLink: '',    
+imageLinks: ['https://res.cloudinary.com/dh41vh9dx/image/upload/v1571777311/Screen_Shot_2019-10-22_at_4.46.59_PM.png'],
+    description:
+      'This app uses the Github API to allow users to search for a specific respository. Users can filter by license types, number of stars and decide to include forks. Links provide take users to that repo on Github. Written in Typescript',
+    githubLink: 'https://github.com/unachoza/PrivateGithubSearch',
+  },
+  {
+    id: 2,
+    name: '',
+projectLink: '',    
+imageLinks: [],
+    description: '',
+
+    githubLink: '',
+  },
+  {
+    id: 2,
+    name: '',
+projectLink: '',    
+imageLinks: [],
+    description: '',
+
+    githubLink: '',
+  },
+];

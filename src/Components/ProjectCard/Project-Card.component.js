fcfa66d33e2df22ef7b6f7229c6d7779b0f28eb0
arianna choza <unachoza@gmail.com>
2020-02-11 21:31:03 -0500

    fixed project preview images, for multiples and singles

--- src/Components/ProjectCard/Project-Card.component.js
+++ src/Components/ProjectCard/Project-Card.component.js
@@ -4,27 +4,27 @@ import './Project-Card.styles.css';
 const ProjectCard = ({ project }) => {
   console.log('me', project);
   if (project) {
+    const { projectLink, imageLinks, name, description, githubLink } = project;
     return (
       <div>
-        <a href={project.projectLink} target="_blank">
-          <div className="double-img" id="cf">
-            <img className="bottom project-img" src={project.imageLinks[0]} alt="project preview" />
+        <a href={projectLink} target="_blank">
+          {imageLinks.length > 1 ? (
+            <div className="double-img" id="cf">
+              <img className="bottom project-img" src={imageLinks[0]} alt="project preview" />
 
-            <img
-              className="top project-img"
-              src="https://res.cloudinary.com/dh41vh9dx/image/upload/c_scale,h_140,w_230/v1546220914/susan-sarandon.jpg"
-              alt="project preview"
-            />
-          </div>
+              <img className="top project-img" src={imageLinks[1]} alt="project preview" />
+            </div>
+          ) : (
+            <img className="top project-img" src={imageLinks[0]} alt="project preview" />
+          )}
         </a>
         <br />
-        <h2>{project.name}</h2>
-        <p>{project.description}</p>
-        <a href={project.githubLink} target="_blank">
+        <h2>{name}</h2>
+        <p>{description}</p>
+        <a href={githubLink} target="_blank">
           {' '}
           <h3>See My Code </h3>
         </a>
-        <div className="project-card">hey</div>)
       </div>
     );
   }

2cd301153a3682c90edb22580b1cd22c1bb712cf
arianna choza <unachoza@gmail.com>
2020-02-11 21:23:12 -0500

    check for undefined, then map, map works

--- src/Components/ProjectCard/Project-Card.component.js
+++ src/Components/ProjectCard/Project-Card.component.js
@@ -2,34 +2,34 @@ import React from 'react';
 import './Project-Card.styles.css';
 
 const ProjectCard = ({ project }) => {
-  console.log('change', project);
-  // console.log('here at card', project.name);
-  return <div>ptime</div>
-}
-//   return (
-//     <div>
-//       <a href={project.projectLink} target="_blank">
-//         <div className="double-img" id="cf">
-//           <img
-//             className="bottom project-img"
-//             src={project.imageLinks[0]} alt="project preview"
-//           />
+  console.log('me', project);
+  if (project) {
+    return (
+      <div>
+        <a href={project.projectLink} target="_blank">
+          <div className="double-img" id="cf">
+            <img className="bottom project-img" src={project.imageLinks[0]} alt="project preview" />
 
-//           {/* <img
-//             className="top project-img"
-//             src="https://res.cloudinary.com/dh41vh9dx/image/upload/c_scale,h_140,w_230/v1546220914/susan-sarandon.jpg" alt="project preview"
-//           /> */}
-//         </div>
-//       </a>
-//       <br />
-//       <h2>{project.name}</h2>
-//                         <p>{project.description}</p>
-//                         <a href={project.githubLink}
-//                             target="_blank"> <h3>See My Code </h3></a>
-//       <div className="project-card">hey</div>)
-//     </div>
-//   )
-// };
+            <img
+              className="top project-img"
+              src="https://res.cloudinary.com/dh41vh9dx/image/upload/c_scale,h_140,w_230/v1546220914/susan-sarandon.jpg"
+              alt="project preview"
+            />
+          </div>
+        </a>
+        <br />
+        <h2>{project.name}</h2>
+        <p>{project.description}</p>
+        <a href={project.githubLink} target="_blank">
+          {' '}
+          <h3>See My Code </h3>
+        </a>
+        <div className="project-card">hey</div>)
+      </div>
+    );
+  }
+  return <div>Name</div>;
+};
 
 export default ProjectCard;
 
